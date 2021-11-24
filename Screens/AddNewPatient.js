3a7ba93269e87ff67ebf732d9e57ecd57022b996
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2021-11-24 20:10:09 +0330

    New features has been added to AddNewPatient.js

--- Screens/AddNewPatient.js
+++ Screens/AddNewPatient.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { StyleSheet,TextInput,View, Text, TouchableHighlight,Button,Alert,TouchableOpacity} from 'react-native'
 import {GlobalStyles} from '../Style/Global';
+import ViewPatientScreen from '../Screens/ViewPatientScreen';
+
+
 import {useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+// import {createNativeStackNavigator} from 'react-navigation/native-stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DatePicker from 'react-native-neat-date-picker'
 
 
-const AddNewPatient = ({navigation, route})=> {
+const AddNewPatient = ({navigation, route})=>{
+  
   var [firstName, onChangeText] = React.useState('');
   var [lastName, onChangeText2] = React.useState('');
   var [doctor, onChangeText3] = React.useState('');
@@ -16,45 +21,57 @@ const AddNewPatient = ({navigation, route})=> {
   var [address, onChangeText6] = React.useState('');
   var [servicePlan, onChangeText7] = React.useState('');
   var [phone, onChangeText8] = React.useState('');
-  // const [count, setCount] = React.useState(0);
-  // const onPress = () => setCount(prevCount => lastName / (firstName/100) / (firstName/ 100));
-  const [cancelPressed, setCancel] = React.useState(0);
-  // const onPress = () => setCancel(prevCount => lastName = "");
-  const onPress = () => {
-    setCancel(prevCount => lastName = "")
+  const clearAll = () => {
+    onChangeText('');
+    onChangeText2('');
+    onChangeText3('');
+    onChangeText4('');
+    onChangeText5('');
+    onChangeText6('');
+    onChangeText7('');
+    onChangeText8('');
+  }
+  const cancelAll = () => {
+    clearAll();
+    alert("Canceled");
   }
 
-    const [showDatePicker, setShowDatePicker] = useState(false)
+  const [showDatePicker, setShowDatePicker] = useState(false)
 
 
   const addPatinet = async () => {
-
-
+	
+	
     // POST request using fetch with async/await
     const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          firstName: firstName,
-          lastName: lastName,
-          dateOfBirth: dateOfBirth,
-          doctor: doctor,
-          department: department,
-          servicePlan: servicePlan,
-          address: address,
-          phone: phone
-        })
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        dateOfBirth: dateOfBirth,
+        doctor: doctor,
+        department: department,
+        servicePlan: servicePlan,
+        address: address,
+        phone: phone
+      })
     };
     try{
 
-    const response = await fetch('http://127.0.0.1:5000/patients', requestOptions).
-    catch(err => console.log(err));
-
-  } catch (error) {
-    console.error(error);
+      const response = await fetch('http://127.0.0.1:5000/patients', requestOptions).catch(err => console.log(err));
+      // console.log(response._bodyBlob)
+      // console.log(response)
+      // const data = await response.json();
+      // console.log(data)
+    } catch (error) {
+      alert("Error in server connection!");
+      console.error(error);
+    }
+    clearAll();
+    alert("New patient has been added");
+    navigation.navigate("View Patients", {msg: 'New patient has been added successfully'});
   }
- 
-}
   const openDatePicker = () => {
     setShowDatePicker(true)
   }
@@ -67,139 +84,142 @@ const AddNewPatient = ({navigation, route})=> {
   const onConfirm = ( date ) => {
     // You should close the modal in here
     setShowDatePicker(false)
-
+    onChangeText5(date.toString().split(" 00:00", 1).toString())
     // The parameter 'date' is a Date object so that you can use any Date prototype method.
     console.log(date.getDate())
+
   }
   return(
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View style={[GlobalStyles.textButtonRowContainer]}>
-        <Text>First Name:        </Text>
-        <TextInput
-          style={[GlobalStyles.textInputContainer]}
-          placeholder="Enter your first name"
-          placeholderTextColor="gray"
-          onChangeText={text => onChangeText(text)}
-          value={firstName}
-        />
-      </View>
-      <View style={[GlobalStyles.textButtonRowContainer]}>
-        <Text>Last Name:        </Text>
-        <TextInput
-          style={[GlobalStyles.textInputContainer]}
-          placeholder="Enter your last name"
-          placeholderTextColor="gray"
-          onChangeText={text => onChangeText2(text)}
-          value={lastName}
-        />
-      </View>
-      <View style={[GlobalStyles.textButtonRowContainer]}>
-        <Text>Doctor:               </Text>
-        <TextInput
-          style={[GlobalStyles.textInputContainer]}
-          placeholder="Enter your doctor name"
-          placeholderTextColor="gray"
-          onChangeText={text => onChangeText3(text)}
-          value={doctor}
-        />
-      </View>
-      <View style={[GlobalStyles.textButtonRowContainer]}>
-        <Text>Department:       </Text>
-        <TextInput
-          style={[GlobalStyles.textInputContainer]}
-          placeholder="Enter your department name"
-          placeholderTextColor="gray"
-          onChangeText={text => onChangeText4(text)}
-          value={department}
-        />
-      </View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+<View style={[GlobalStyles.textButtonRowContainer]}>
+    <Text>First Name:        </Text>
+  <TextInput
+  style={[GlobalStyles.textInputContainer]}
+  placeholder="Enter your first name"
+  placeholderTextColor="gray"
+  onChangeText={text => onChangeText(text)}
+  value={firstName}
+  />
+  </View>
+  <View style={[GlobalStyles.textButtonRowContainer]}>
+    <Text>Last Name:        </Text>
+  <TextInput
+  style={[GlobalStyles.textInputContainer]}
+  placeholder="Enter your last name"
+  placeholderTextColor="gray"
+  onChangeText={text => onChangeText2(text)}
+  value={lastName}
+  />
+  </View>
+  <View style={[GlobalStyles.textButtonRowContainer]}>
+    <Text>Doctor:               </Text>
+  <TextInput
+  style={[GlobalStyles.textInputContainer]}
+  placeholder="Enter your doctor name"
+  placeholderTextColor="gray"
+  onChangeText={text => onChangeText3(text)}
+  value={doctor}
+  />
+  </View>
+  <View style={[GlobalStyles.textButtonRowContainer]}>
+    <Text>Department:       </Text>
+  <TextInput
+  style={[GlobalStyles.textInputContainer]}
+  placeholder="Enter your department name"
+  placeholderTextColor="gray"
+  onChangeText={text => onChangeText4(text)}
+  value={department}
+  />
+  </View>
 
 
-      <View style={[GlobalStyles.textButtonRowContainer]}>
-        <Text>servicePlan:        </Text>
-        <TextInput
-          style={[GlobalStyles.textInputContainer]}
-          placeholder="Enter service plan"
-          placeholderTextColor="gray"
-          onChangeText={text => onChangeText7(text)}
-          value={servicePlan}
-        />
-      </View>
+  <View style={[GlobalStyles.textButtonRowContainer]}>
+    <Text>servicePlan:        </Text>
+  <TextInput
+  style={[GlobalStyles.textInputContainer]}
+  placeholder="Enter service plan"
+  placeholderTextColor="gray"
+  onChangeText={text => onChangeText7(text)}
+  value={servicePlan}
+  />
+  </View>
 
-      <View style={[GlobalStyles.textButtonRowContainer]}>
-        <Text>Date of Birth:    </Text>
-        <TextInput
-          style={[GlobalStyles.textInputContainer]}
-          placeholder="             /            /            "
-          placeholderTextColor="gray"
-          onChangeText={text => onChangeText5(text)}
-          value={dateOfBirth}
-        />
-                      <View style={{width:"12%",
-        backgroundColor:"orange",
-        height:15,
-        alignItems:"center",
-        justifyContent:"center",
-        margin: 5}}>
+  <View style={[GlobalStyles.textButtonRowContainer]}>
+    <Text>Date of Birth:    </Text>
+  <TextInput
+  style={[GlobalStyles.textInputContainer]}
+  placeholder="             /            /            "
+  placeholderTextColor="gray"
+  onChangeText={text => onChangeText5(text)}
+  value={dateOfBirth}
+  />
+  <View style={{width:"12%",
+    backgroundColor:"orange",
+    height:15,
+    alignItems:"center",
+    justifyContent:"center",
+    margin: 5}}>
 
-        <TouchableOpacity onPress={openDatePicker}>
+<TouchableOpacity onPress={openDatePicker}>
 
-          <Text>Pick</Text>
-              <DatePicker
-        isVisible={showDatePicker}
-        mode={'single'}
-        onCancel={onCancel}
-        onConfirm={onConfirm}
-      />
-        </TouchableOpacity>
-
-
-            </View>
-      </View>
-      <View style={[GlobalStyles.textButtonRowContainer]}>
-        <Text>Address:              </Text>
-        <TextInput
-          style={[GlobalStyles.textInputContainer]}
-          placeholder="Enter your address"
-          placeholderTextColor="gray"
-          onChangeText={text => onChangeText6(text)}
-          value={address}
-        />
-      </View>
-
-      <View style={[GlobalStyles.textButtonRowContainer]}>
-        <Text>Phone:                 </Text>
-        <TextInput
-          style={[GlobalStyles.textInputContainer]}
-          placeholder="Enter your phone"
-          placeholderTextColor="gray"
-          onChangeText={text => onChangeText8(text)}
-          value={phone}
-        />
-      </View>
+    <Text>Pick</Text>
+    <DatePicker
+  isVisible={showDatePicker}
+  mode={'single'}
+  onCancel={onCancel}
+  onConfirm={onConfirm}
+  />
+  
+  </TouchableOpacity>
 
 
-      <View style={{flex: 1 , flexDirection: 'row' }}>
-              <View style={[GlobalStyles.appButtonContainer ,  {flex: 1 ,flexDirection:'column' }]}>
-        <TouchableOpacity
-          onPress={addPatinet}
-        >
-          <Text style = {GlobalStyles.appButtonText}>Add</Text>
-        </TouchableOpacity>
-        </View>
+  </View>
+  </View>
+  <View style={[GlobalStyles.textButtonRowContainer]}>
+    <Text>Address:              </Text>
+  <TextInput
+  style={[GlobalStyles.textInputContainer]}
+  placeholder="Enter your address"
+  placeholderTextColor="gray"
+  onChangeText={text => onChangeText6(text)}
+  value={address}
+  />
+  </View>
 
-              <View style={[GlobalStyles.appButtonContainer ,  {flex: 1 ,flexDirection:'column' }]}>
+  <View style={[GlobalStyles.textButtonRowContainer]}>
+    <Text>Phone:                 </Text>
+  <TextInput
+  style={[GlobalStyles.textInputContainer]}
+  placeholder="Enter your phone"
+  placeholderTextColor="gray"
+  onChangeText={text => onChangeText8(text)}
+  value={phone}
+  />
+  </View>
 
 
-                  <TouchableOpacity onPress={onPress}>
-          <Text style = {GlobalStyles.appButtonText}>Cancel</Text>
-        </TouchableOpacity>
-        </View>
-
-      </View>
+  <View style={{flex: 1 , flexDirection: 'row' }}>
+<View style={[GlobalStyles.appButtonContainer ,  {flex: 1 ,flexDirection:'column' }]}>
+<TouchableOpacity
+  onPress={addPatinet}
+    >
+    <Text style = {GlobalStyles.appButtonText}>Add</Text>
+    </TouchableOpacity>
     </View>
-  );
+
+    <View style={[GlobalStyles.appButtonContainer ,  {flex: 1 ,flexDirection:'column' }]}>
+
+
+<TouchableOpacity onPress={cancelAll}>
+    <Text style = {GlobalStyles.appButtonText}>Cancel</Text>
+    </TouchableOpacity>
+    </View>
+
+    </View>
+    </View>
+);
 }
+
 
 
 export default AddNewPatient;
