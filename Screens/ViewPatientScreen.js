02949b2029beee08d016f478205968aff7b33b09
Al <roal.24.alro@gmail.com>
2021-12-02 16:27:28 -0500

    4

--- Screens/ViewPatientScreen.js
+++ Screens/ViewPatientScreen.js
@@ -22,7 +22,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
        />
       </View>
       <View style={{flex: 1, flexDirection: 'row' }}>
-    <Text style={[GlobalStyles.titleText, {flex: 1 ,flexDirection:'column' }]}>LastName:</Text>
+    <Text style={[GlobalStyles.titleText, {flex: 1 ,flexDirection:'column' }]}>Last Name:</Text>
     <TextInput
          style={GlobalStyles.textInputStyles}
        />
@@ -42,121 +42,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
     <View style={[GlobalStyles.contentflex , {flex: 5 }]} >
 
-
-    <View>
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title style={{flex: 1}}>#</DataTable.Title>
-          <DataTable.Title style={{flex: 2}}>Name</DataTable.Title>
-          <DataTable.Title style={{flex: 1}}>Age </DataTable.Title>
-          <DataTable.Title style={{flex: 2}}>HealthId</DataTable.Title>
-          <DataTable.Title style={{flex: 2}}></DataTable.Title>
-          <DataTable.Title style={{justifyContent:"flex-end", flex: 2}}></DataTable.Title>
-          <DataTable.Title style={{justifyContent:"flex-end", flex: 2}}></DataTable.Title>
-        </DataTable.Header>
-        <DataTable.Row >
-        <DataTable.Cell style={{flex: 1}}>
-              <Text style={GlobalStyles.recordRow}>1</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}}>
-              <Text style={GlobalStyles.recordRow}>Roud.G</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 1}} numeric>
-              <Text style={GlobalStyles.recordRow}>40</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Text style={GlobalStyles.recordRow}>234</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Button style={GlobalStyles.recordRow} title="Add" onPress={()=>{
-                    this.props.navigation.navigate('Add Patient Record',
-                    {
-                        patientId: '61a128da71232b9fb3b61ff7',
-                        otherParam: 'anything you want here',
-                        });
-                    }}
-                />
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Button style={GlobalStyles.recordRow} title="List" onPress={()=>{
-                    this.props.navigation.navigate('View Patient Record', {
-                        patientId: '61a128da71232b9fb3b61ff7',
-                        otherParam: 'anything you want here',
-                        });
-                    }}
-                />
-          </DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row >
-
-        <DataTable.Cell style={{flex: 1}}>
-              <Text style={GlobalStyles.recordRow}>2</Text>
-          </DataTable.Cell>
-
-          <DataTable.Cell style={{flex: 2}}>
-              <Text style={GlobalStyles.recordRow}>Botton.M</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 1}} numeric>
-              <Text style={GlobalStyles.recordRow}>38</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Text style={GlobalStyles.recordRow}>123</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Button style={GlobalStyles.recordRow} title="Add" onPress={()=>this.props.navigation.navigate('Add Patient Record')}></Button>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Button style={GlobalStyles.recordRow} title="List" onPress={()=>this.props.navigation.navigate('View Patient Record')}></Button>
-          </DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row >
-        <DataTable.Cell style={{flex: 1}}>
-              <Text style={GlobalStyles.recordRow}>3</Text>
-          </DataTable.Cell>
-
-          <DataTable.Cell style={{flex: 2}}>
-              <Text style={GlobalStyles.recordRow}>Sancho.W</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 1}} numeric>
-              <Text style={GlobalStyles.recordRow}>28</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Text style={GlobalStyles.recordRow}>678</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Button style={GlobalStyles.recordRow} title="Add" onPress={()=>this.props.navigation.navigate('Add Patient Record')}></Button>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Button style={GlobalStyles.recordRow} title="List" onPress={()=>this.props.navigation.navigate('View Patient Record')}></Button>
-          </DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row >
-
-         <DataTable.Cell style={{flex: 1}}>
-              <Text style={GlobalStyles.recordRow}>4</Text>
-          </DataTable.Cell>
-
-          <DataTable.Cell style={{flex: 2}}>
-              <Text style={GlobalStyles.recordRow}>Sancho.W</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 1}} numeric>
-              <Text style={GlobalStyles.recordRow}>28</Text>
-          </DataTable.Cell>
-          <DataTable.Cell style={{flex: 2}} numeric>
-              <Text style={GlobalStyles.recordRow}>234</Text>
-          </DataTable.Cell>
-            <DataTable.Cell style={{flex: 2}} numeric>
-                <Button style={GlobalStyles.recordRow} title="Add" onPress={()=>this.props.navigation.navigate('Add Patient Record')}></Button>
-            </DataTable.Cell>
-            <DataTable.Cell style={{flex: 2}} numeric>
-                <Button style={GlobalStyles.recordRow} title="List" onPress={()=>this.props.navigation.navigate('View Patient Record')}></Button>
-            </DataTable.Cell>
-        </DataTable.Row>
-
-        </DataTable>
-        </View>
-
-
+    <View style={[GlobalStyles.patientData , {backgroundColor: '#ABD3EC' }]}>
+       {
+         this.state.patients.length > 0
+         ? <PatientList patients={this.state.patients}/>
+         : <Text style={styles.header}>No records available!</Text>
+       }
+   </View>
+   
     </View>
 
     </View>
